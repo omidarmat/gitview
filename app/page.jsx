c55bb1e3b2c2385315ece8fc985ee4d94d18dbfb
Omid Armat <omidarmat@gmail.com>
2024-10-12 00:16:11 +0330

    implement home init with session detect

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,23 @@
-export default function Home() {
-  return <p>Home</p>;
+import { cookies } from "next/headers";
+import Link from "next/link";
+
+export default async function Home() {
+  const cookieStore = cookies();
+  const session = cookieStore.get("gitview-session");
+
+  return (
+    <div>
+      {!session && (
+        <>
+          <p>please log in to see your repos</p>
+          <Link
+            href={`https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}&state=${process.env.STATE}`}
+          >
+            Login with github
+          </Link>
+        </>
+      )}
+      {session && <p>Loading your repos...</p>}
+    </div>
+  );
 }
