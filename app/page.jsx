37ba6989fd39524c14be18a441220ca87c3c0e08
Omid Armat <omidarmat@gmail.com>
2024-10-12 23:55:03 +0330

    implement media queries and other styles

--- app/page.jsx
+++ app/page.jsx
@@ -7,28 +7,30 @@ import { redirect } from "next/navigation";
 export default async function Home() {
   const JSX = (
     <main>
-      <div className="bg-primary-100 my-auto px-5 pt-12 py-7 flex flex-col gap-2 items-center text-center rounded-2xl max-h-dvh">
+      <div className="bg-primary-100 px-5 pt-12 py-7 flex flex-col gap-2 items-center justify-center text-center rounded-2xl h-[95vh]">
         <Image
           src="/gitview-logo.png"
           width={100}
           height={100}
           alt="Gitview logo"
-          className="mb-5"
+          className="mb-5 sm:w-34"
         />
-        <h1 className="text-xl font-bold text-text-heading">Gitview</h1>
-        <p className="text-sm text-text-heading">
+        <h1 className="text-xl font-bold text-text-heading sm:text-3xl">
+          Gitview
+        </h1>
+        <p className="text-sm text-text-heading sm:text-lg">
           View all your repositories key information in one clean view.
         </p>
-        <p className="mt-10 mb-2 text-sm font-bold text-text-heading">
+        <p className="mt-10 mb-2 text-sm font-bold text-text-heading sm:text-lg">
           Please login to your account
         </p>
         <Link
           href={`https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}&state=${process.env.STATE}`}
-          className="bg-primary-700 text-primary-100 px-6 py-2 rounded-lg mb-10"
+          className="bg-primary-700 text-primary-100 px-6 py-2 rounded-lg mb-10 sm:text-lg hover:bg-primary-400 transition-colors duration-300"
         >
           Login with github
         </Link>
-        <div className="flex flex-col gap-1 text-xs">
+        <div className="flex flex-col gap-1 text-xs sm:text-base">
           <p>Gitview 1.0.0</p>
           <p>&copy;copyright 2024 by Omid Armat</p>
         </div>
--- app/repos/page.jsx
+++ app/repos/page.jsx
@@ -26,27 +26,30 @@ async function Repos({ searchParams }) {
   return (
     <>
       <header className="flex items-center justify-between mb-7">
-        <div className="flex gap-2 items-center">
+        <div className="flex gap-2 sm:gap-4 items-center">
           <Image
             src="/gitview-logo.png"
             height={45}
             width={45}
             alt="Gitview logo"
+            className="sm:w-20"
           />
           <div>
-            <h1 className="font-bold text-lg text-text-heading">Gitview</h1>
-            <p className="text-xs">v 1.0.0</p>
+            <h1 className="font-bold text-lg text-text-heading sm:text-3xl">
+              Gitview
+            </h1>
+            <p className="text-xs sm:text-lg">v 1.0.0</p>
           </div>
         </div>
 
-        <a href="/?logout=1" className="icon-button">
+        <a href="/?logout=1" className="icon-button sm:text-2xl">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             strokeWidth={2}
             stroke="currentColor"
-            className="size-5"
+            className="size-5 sm:size-7"
           >
             <path
               strokeLinecap="round"
@@ -57,15 +60,14 @@ async function Repos({ searchParams }) {
           <p>Logout</p>
         </a>
       </header>
-      <main className="flex flex-col h-[94vh]">
-        {!repos && <div>Something went wrong!</div>}
+      <main className="flex flex-col h-dvh relative">
         {repos && (
           <div className="text-text-normal mb-10">
-            <div className="mb-5">
-              <h2 className="text-text-heading text-lg font-semibold">
+            <div className="mb-5 sm:mb-6">
+              <h2 className="text-text-heading text-lg font-semibold sm:text-2xl sm:mb-2">
                 {fullname}&#39;s repositories
               </h2>
-              <p className="text-sm">
+              <p className="text-sm sm:text-lg">
                 Found{" "}
                 <span className="text-primary-400 font-semibold">
                   {repos.length}
@@ -84,9 +86,9 @@ async function Repos({ searchParams }) {
                       key={repo.id}
                       className="bg-primary-100 rounded-xl px-5 py-4"
                     >
-                      <div className="flex items-start mb-4">
+                      <div className="flex items-start justify-between mb-4">
                         <div>
-                          <h3 className="font-semibold mb-1 text-lg">
+                          <h3 className="font-semibold mb-1 text-lg sm:text-2xl">
                             {repo.name}
                           </h3>
                           <div className="flex gap-2 items-center">
@@ -97,8 +99,10 @@ async function Repos({ searchParams }) {
                               height={35}
                               className="rounded-full"
                             />
-                            <p className="font-semibold text-sm">
-                              <span className="text-xs">owned by</span>{" "}
+                            <p className="font-semibold text-sm sm:text-lg">
+                              <span className="text-xs sm:text-base sm:font-normal">
+                                owned by
+                              </span>{" "}
                               {repo.owner.login}
                             </p>
                           </div>
@@ -106,7 +110,7 @@ async function Repos({ searchParams }) {
 
                         <a
                           href={`https://codeload.github.com/${repo.owner.login}/${repo.name}/zip/refs/heads/main`}
-                          className="flex items-center gap-2 bg-primary-700 px-3 py-2 rounded-xl text-primary-100 text-sm font-bold"
+                          className="flex items-center gap-2 sm:gap-3 bg-primary-700 px-3 py-2 rounded-xl text-primary-100 text-sm font-bold transition-colors duration-300 hover:bg-primary-400"
                         >
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -114,7 +118,7 @@ async function Repos({ searchParams }) {
                             viewBox="0 0 24 24"
                             strokeWidth={2}
                             stroke="currentColor"
-                            className="size-4"
+                            className="size-4 sm:size-6"
                           >
                             <path
                               strokeLinecap="round"
@@ -122,16 +126,16 @@ async function Repos({ searchParams }) {
                               d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
                             />
                           </svg>
-                          <p>Download</p>
+                          <p className="sm:text-lg">Download</p>
                         </a>
                       </div>
-                      <div className="flex gap-1 items-center">
+                      <div className="flex gap-1 sm:gap-2 items-center">
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           fill="none"
                           viewBox="0 0 24 24"
                           strokeWidth={1.5}
-                          className="size-4 stroke-primary-400"
+                          className="size-4 sm:size-5 stroke-primary-400"
                         >
                           <path
                             strokeLinecap="round"
@@ -140,7 +144,7 @@ async function Repos({ searchParams }) {
                           />
                         </svg>
 
-                        <p className="text-xs">
+                        <p className="text-xs sm:text-base">
                           Last update:{" "}
                           {new Date(repo.updated_at).toDateString()}
                         </p>
@@ -170,7 +174,7 @@ async function Repos({ searchParams }) {
                       viewBox="0 0 24 24"
                       strokeWidth={1.5}
                       stroke="currentColor"
-                      className="size-4"
+                      className="size-4 sm:size-6"
                     >
                       <path
                         strokeLinecap="round"
@@ -178,12 +182,12 @@ async function Repos({ searchParams }) {
                         d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
                       />
                     </svg>
-                    <p>page {+currentPage - 1}</p>
+                    <p className="sm:text-lg">page {+currentPage - 1}</p>
                   </button>
                 </form>
               ) : null}
 
-              <p className="text-sm">
+              <p className="text-sm sm:text-lg">
                 Page{" "}
                 <span className="text-primary-400 font-bold">
                   {currentPage}
@@ -205,14 +209,14 @@ async function Repos({ searchParams }) {
                   }}
                 >
                   <button type="submit" className="icon-button">
-                    <p>page {+currentPage + 1}</p>
+                    <p className="sm:text-lg">page {+currentPage + 1}</p>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
                       strokeWidth={1.5}
                       stroke="currentColor"
-                      className="size-4"
+                      className="size-4 sm:size-6"
                     >
                       <path
                         strokeLinecap="round"
@@ -226,7 +230,7 @@ async function Repos({ searchParams }) {
             </div>
           </div>
         )}
-        <p className="text-center text-xs mt-auto pb-8">
+        <p className="mx-auto text-xs sm:text-base">
           &copy;Copyright 2024 by Omid Armat
         </p>
       </main>
